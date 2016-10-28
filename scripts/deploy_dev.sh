73abab2349b1a85ac537f2ac1b08bfbefd6c240f
Abdullah Al Maruf <maruf@Abdullahs-MacBook-Pro.local>
2016-10-28 15:26:07 +0900

    sls --> serverless

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -4,7 +4,7 @@ STAGE=dev
 
 OPTION="-s $STAGE -r $AWS_REGION"
 
-sls project init -p $AWS_PROFILE $OPTION
-sls resources deploy $OPTION
-sls function deploy $OPTION
-sls endpoint deploy $OPTION
+serverless project init -p $AWS_PROFILE $OPTION
+serverless resources deploy $OPTION
+serverless function deploy $OPTION
+serverless endpoint deploy $OPTION
